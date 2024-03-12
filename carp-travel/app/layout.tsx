89ad19ae7f692801fabfbd2340e92dfd46f37b5a
chemvic <chemvic79@gmail.com>
2024-03-12 18:08:07 +0200

    meta

--- carp-travel/app/layout.tsx
+++ carp-travel/app/layout.tsx
@@ -18,8 +18,15 @@ export default function RootLayout({
   return (
     <html lang="en">
        <Head>
-          {/* <link rel="icon" href="/favicon.svg" type="image/svg+xml" /> */}
-          <link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="48x48" />
+          <link rel="icon" href="/favicon.svg" type="image/svg+xml" sizes="48x48" />
+          {/* <link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="48x48" /> */}
+          <meta name="keywords"
+      content="carpatians, mountains, rafting, ballooning, climbing"/>
+    <meta property="og:title" content="Uncover Carpathian’s Secrets"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:locale" content="en"/>
+    <meta property="og:description" content="Internet page of the service for selecting active leisure in the Carpathians"/>
+    <meta property="og:url" content=""/>
         </Head>
       <body className="text-white relative body">
         <Header/>
